933629cbd01805fb05fe702842b86b97d3bc864c
Matt Stirling <stirlingmattmarkus@gmail.com>
2025-01-21 15:24:49 +0200

    initial commit

---
+++ distribute.sh
@@ -0,0 +1,16 @@
+
+# make sure packages are installed and up to date
+python3 -m pip install -U pip
+python3 -m pip install -U build
+python3 -m pip install -U twine
+python3 -m pip install -U keyring
+
+# remove old build files and build
+rm -rf dist/*
+python3 -m build
+
+
+# upload
+#python3 -m twine upload --repository testpypi dist/*
+python3 -m twine upload dist/*
+
