23a6d51d5c6a1e219b3bae7560907e1203defc9e
Matt Stirling <stirlingmattmarkus@gmail.com>
2025-01-26 18:29:29 +0200

    added more fonts, need to fix size determining

--- src/ascii_monet/ascii_monet.py
+++ src/ascii_monet/ascii_monet.py
@@ -1,33 +1,47 @@
 import os
+import shutil
 import math
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 from colorama import just_fix_windows_console, init
 just_fix_windows_console() # gets ANSI working in Windows CMD
-init(strip=False)
+# init(strip=False) 
 
 class ascii_monet:
     
+    fonts = [
+        "assets/Anonymous.ttf",
+        "assets/fonts/iAWriterDuospace-Regular.otf",
+        "assets/fonts/CascadiaMono-Regular.otf",
+    ]
+    font_path = os.path.join(os.path.dirname(__file__), fonts[-1])
+    
+    
     @classmethod
-    def generate(cls, path):
+    def generate(cls, path, custom_chars=None, only_alphanum=False, only_alpha=False, terminal_width=False, height=None, width=80, max_height=100, max_width=None):
         """ Given a path to an image, generates colored ASCII art of the image in the terminal """
 
         image_path = os.path.join(os.curdir, path)
-        # image_path = os.path.abspath(os.path.join(os.curdir, path))
         if not os.path.exists(image_path):
             print('ERROR: Path does not exist: "{}"'.format(image_path))
             return 1
         
         print('Generating ASCII art of image: "{}"'.format(image_path))
-        print("\033[38;2;0;0;0mThis should be black text\033[0m")
+        # print("\033[38;2;0;0;0mThis should be black text\033[0m")
+        
+        chars = cls.getCharsToUse(custom_chars=custom_chars, only_alphanum=only_alphanum, only_alpha=only_alpha)
+        # print(chars)
         
         image = np.array(Image.open(image_path))
-        block_size = np.astype(np.array((20,9)) * 1.5, int)
+        block_size = cls.getBlockSize(image.shape, terminal_width, height, width, max_height, max_width)
         blocks = cls.toBlocks(image, block_size)
+
+        # get average block pixel values
+        colors = []
         for row in blocks:
             for char in row:
                 pixel_mean = np.astype(np.mean(char, axis=(0, 1)), np.uint8)
-                char = cls.getChar(pixel_mean)
+                char = cls.getChar(pixel_mean, chars)
                 cls.print_colored_text(char, pixel_mean, end='')
             print()
 
@@ -61,6 +75,64 @@ class ascii_monet:
                 blocks[J][I] = block
         return blocks
     
+    @classmethod
+    def getChar(cls, px_mean, chars):
+        luminance = np.mean(px_mean)
+        idx = math.floor(cls.map_value(luminance, 0, 255, 0, len(chars)-1))
+        return chars[idx]
+    
+
+    @classmethod
+    def getCharsToUse(cls, custom_chars=None, only_alphanum=False, only_alpha=False):
+        chars_upper = [ chr(x) for x in range(65, 91) ]
+        chars_lower = [ chr(x) for x in range(97, 123) ]
+        chars_digits = [ str(x) for x in range(10) ]
+        chars_all = [ chr(x) for x in range(33, 127) ]
+
+        chars = chars_all
+        if custom_chars:
+            chars = custom_chars
+        elif only_alphanum:
+            chars = chars_upper + chars_lower + chars_digits
+        elif only_alpha:
+            chars = chars_upper + chars_lower
+        chars.sort(key=lambda ch: cls.get_on_off_pixel_ratio(ch, cls.font_path))
+        return chars
+    
+    @classmethod
+    def getBlockSize(cls, img_shape, terminal_width, height, width, max_height, max_width):
+        block_size_default = (20,9)
+        terminal_height, terminal_width = shutil.get_terminal_size()
+
+        mult = 2
+        
+        return np.astype(np.array(block_size_default)*mult, int)
+    
+    #region ###########################  STATIC METHODS #####################  
+    
     @staticmethod
-    def getChar(px_mean):
-        return '0'
+    def map_value(x, a_min, a_max, b_min, b_max):
+        return b_min + (x - a_min) * (b_max - b_min) / (a_max - a_min)
+    
+    
+    @staticmethod
+    def get_on_off_pixel_ratio(char, font_path):
+        font_size = 24
+        
+        ar = 7/12
+        img = Image.new('1', (int(font_size*ar), font_size), 0) # '1' mode is 1-bit pixels
+        draw = ImageDraw.Draw(img)
+
+        font = ImageFont.truetype(font_path, font_size)
+        draw.text((0, 0), char, font=font, fill=1)
+
+        pixels = list(img.getdata())
+        on_pixels = pixels.count(1)
+        off_pixels = pixels.count(0)
+
+        total_pixels = on_pixels + off_pixels
+        if total_pixels == 0:
+            return 0
+        return on_pixels / total_pixels
+    
+    #endregion ###########################  END STATIC METHODS #############  
--- src/ascii_monet/cli.py
+++ src/ascii_monet/cli.py
@@ -1,5 +1,4 @@
 import argparse
-import shutil
 from pathlib import Path
 from ascii_monet.ascii_monet import ascii_monet
 
@@ -9,7 +8,18 @@ def main():
     )
     parser.add_argument("image_path", nargs="?", help="Path to the image file")
     parser.add_argument('--grayscale', '-g', action='store_true', help='Prints image in grayscale')
-    parser.add_argument('--background-color', '-bc', help='Prints image in grayscale')
+    # parser.add_argument('--background-color', '-bc', help='')
+    parser.add_argument('--white-background', action='store_true', help='Assumes white background when selecting chars')
+    parser.add_argument('--terminal-width', action='store_true', help='')
+    parser.add_argument('--terminal-height', action='store_true', help='')
+    parser.add_argument('--width', help='', type=int)
+    parser.add_argument('--height', help='', type=int)
+    parser.add_argument('--max-width', help='', type=int)
+    parser.add_argument('--max-height', help='', type=int)
+    parser.add_argument('--char-aspect-ratio', help='Aspect ratio of chars given as a string as "H:W"')
+    parser.add_argument('--chars-to-use', help='Give a list of chars to use in the image as a string')
+    parser.add_argument('--only-alpha-numeric', action='store_true', help='Only use alpha-numeric characters')
+    parser.add_argument('--only-alpha', action='store_true', help='Only use alphabetic characters')
 
     args = parser.parse_args()
 
@@ -17,15 +27,10 @@ def main():
         print("Usage: ascii-monet <image-path> <OPTIONS>")
         exit(1)
 
-    # get script dir
-    scriptdir = Path(__file__).resolve().parent
-    # print(scriptdir)
-    
-    terminal_width = shutil.get_terminal_size().columns
-    print(terminal_width)
+    chars_to_use = None if not args.chars_to_use else list(args.chars_to_use)
 
     print()
-    ret = ascii_monet.generate(args.image_path)
+    ret = ascii_monet.generate(args.image_path, custom_chars=chars_to_use, only_alphanum=args.only_alpha_numeric, only_alpha=args.only_alpha)
     print()
     exit(ret)
 
